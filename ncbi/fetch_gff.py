851ccc6c41c81e0db8c7f742ce6116ee4b545499
Hamid Bagheri <bagheri.hamid@gmail.com>
2017-06-29 18:01:32 -0500

    add assembly stats

--- ncbi/fetch_gff.py
+++ ncbi/fetch_gff.py
@@ -12,15 +12,19 @@ with open ('assembly_summary.txt') as f:
         info=line.split('\t')
         fna_url=info[19]+'/'+info[19].split('/')[-1]+'_genomic.fna.gz'
         fnafileName=info[19].split('/')[-1]+'_genomic.fna.gz'
+
+        assembly_url=info[19]+'/'+info[19].split('/')[-1]+'_assembly_stats.txt'
+        assembly_stat= info[19].split('/')[-1]+'_assembly_stats.txt'
+
         gff_url=info[19]+'/'+info[19].split('/')[-1]+'_genomic.gff.gz'
         gfffileName=info[19].split('/')[-1]+'_genomic.gff.gz'
+        
         print(gfffileName)
         if not os.path.isfile(fnafileName):
-            os.system('curl -o ' + fnafileName + ' '  + fna_url)
-            os.system('curl -o ' + gfffileName + ' '  + gff_url)
-            i+=1
-        if i==10000:
-            break
+            #os.system('curl -o ' + fnafileName + ' '  + fna_url)
+            #os.system('curl -o ' + gfffileName + ' '  + gff_url)
+            os.system('curl ' + assembly_url + '>>' + assembly_stat )
+            
 
 
 
