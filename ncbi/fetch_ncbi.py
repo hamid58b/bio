9e9f80c46a522294a05c1a0b512b0645d16c336d
Hamid Bagheri <bagheri.hamid@gmail.com>
2017-06-30 01:02:16 -0500

    add assembly url to download

--- ncbi/fetch_ncbi.py
+++ ncbi/fetch_ncbi.py
@@ -16,12 +16,18 @@ def dl_from_ncbi(assembly_summary):
             info=line.split('\t')
             fna_url=info[19]+'/'+info[19].split('/')[-1]+'_genomic.fna.gz'
             fnafileName=info[19].split('/')[-1]+'_genomic.fna.gz'
+            
+            assembly_url=info[19]+'/'+info[19].split('/')[-1]+'_assembly_stats.txt'
+            assembly_stat= info[19].split('/')[-1]+'_assembly_stats.txt'
+        
             gff_url=info[19]+'/'+info[19].split('/')[-1]+'_genomic.gff.gz'
             gfffileName=info[19].split('/')[-1]+'_genomic.gff.gz'
+            
             print(gfffileName)
             if not os.path.isfile(fnafileName):
                 os.system('curl -o ' + fnafileName + ' '  + fna_url)
                 os.system('curl -o ' + gfffileName + ' '  + gff_url)
+                os.system('curl ' + assembly_url + '>>' + assembly_stat )
                 print (" ################### "+ gfffileName)
 
 
